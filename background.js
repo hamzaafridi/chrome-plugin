ff0ff72bfbab442bd5874a081a309cf1ed3797f5
Hamza Afridi <hamzaafridi@gmail.com>
2020-06-07 09:04:52 +0100

    added more comments for information

--- background.js
+++ background.js
@@ -4,12 +4,12 @@ chrome.runtime.onInstalled.addListener(function() { //event that runs when onIns
     });
     //in this section rules are added to only activate when the pageURL is the one given
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() { //remove rules event when page changes
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
+      chrome.declarativeContent.onPageChanged.addRules([{ //add rule
+        conditions: [new chrome.declarativeContent.PageStateMatcher({ //using api to check page url
           pageUrl: {hostEquals: 'developer.chrome.com'},
         })
         ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
+            actions: [new chrome.declarativeContent.ShowPageAction()] //extention will only show actions when conditions are true
       }]);
     });
   });
